677c4ec9171466b244c1ce53a010677cc1261f4c
Mike DePaul <tars@DellRedux.home>
2017-11-26 22:35:04 -0500

    Initial upload of VS files

---
+++ RandomizedList/IPrng.cs
@@ -0,0 +1,15 @@
+﻿/* Attributions
+ * Mike DePaul
+ * https://github.com/mdepaul/RandomizedList.git
+ * **/
+namespace MD.RandomizedList
+{
+    public interface IPrng
+    {
+        byte GetRandomByte();
+
+        byte[] GetRandomBytes(int numberOfBytes);
+
+        void Reseed();
+    }
+}
